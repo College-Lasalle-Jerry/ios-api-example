772653bae37fbdd0aadf24695d28bef13defcd96
centinna-l <jerryjoy99@gmail.com>
2024-10-28 13:18:07 -0400

    Initial Commit

---
+++ API Example/API_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_ExampleApp.swift
+//  API Example
+//
+//  Created by Jerry Joy on 2024-10-28.
+//
+
+import SwiftUI
+
+@main
+struct API_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
